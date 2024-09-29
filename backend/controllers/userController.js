afbe7620aa75e73579e4aa0374e87bf3c252a45a
tanayyyyy7 <tanayyyyy7@gmail.com>
2024-09-29 15:14:26 +0000

    changed cookie: sameSite to Lax from strict

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,7 +13,7 @@ const setTokenCookie = (res, token, name, maxAge) => {
   res.cookie(name, token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'Lax',
     maxAge: maxAge * 1000
   });
 };
